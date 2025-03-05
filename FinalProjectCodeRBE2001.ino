d56c048b9a0b4d01087028f4eafe99bec3305736
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-03-05 15:52:19 -0500

    Update FinalProjectCodeRBE2001.ino

--- FinalProjectCodeRBE2001.ino
+++ FinalProjectCodeRBE2001.ino
@@ -19,7 +19,7 @@ int blueIncrement = 20; // The amount to increment the blue motor effort by when
 
 // Servo control variables
 Servo32U4Pin5 servo1;
-Servo32U4Pin13 servo2;
+Servo32U4Pin12 servo2;
 int servo1Pos = 1500; // Position of servo 1 (microseconds)
 int servo2Pos = 1500; // Position of servo 2 (microseconds)
 int servoIncrement = 50; // The amount to increment the servo position by (microseconds)
@@ -45,6 +45,7 @@ void loop() {
   // Keyboard controls
   if (Serial.available()) { // If a command is waiting
     char input = Serial.read();
+  
     if (input == 'w') { // Move forward
       leftEffort += dtIncrement;
       rightEffort += dtIncrement;
@@ -83,7 +84,7 @@ void loop() {
       servo2Pos -= servoIncrement;
       servo2.writeMicroseconds(servo2Pos);
     } 
-    else if (input == '{' ){ // Stop the motors if { is pressed
+    else if (input == '{') { // Stop the motors if "{" is pressed
       leftEffort = rightEffort = 0;
       blueEffort = 0;
     }
@@ -93,8 +94,8 @@ void loop() {
     blueMotor.setEffort(blueEffort); // Update blue motor effort. Will not run at less than ~150 effort magnitude
   }
 
-   Serial.println("Servo positions: " + String(servo2Pos) + ", " + String(servo1Pos));
-   //Serial.println("Blue Motor Pos: " + String(blueMotor.getPosition()));
-   //Serial.println("Romi Pos: " + String(chassis.getLeftEncoderCount()) + ", " + String(chassis.getRightEncoderCount()));
-   //Serial.println("Blue Effort: " + String(blueEffort));
+  // Serial.println("Blue Motor Pos: " + String(blueMotor.getPosition()));
+  // Serial.println("Romi Pos: " + String(chassis.getLeftEncoderCount()) + ", " + String(chassis.getRightEncoderCount()));
+  // Serial.println("Blue Effort: " + String(blueEffort));
+  Serial.println(String(servo2Pos) + ", " + String(blueEffort));
 }
